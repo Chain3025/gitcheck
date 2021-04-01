2476d7f5b9b5c5c4ad490c14e89116b2d7887517
chain3 <chain@garageplug.com>
2021-04-01 14:15:46 +0530

    first commit for git hub

---
+++ src/test/java/com/example/gitcheck/GitcheckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitcheck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitcheckApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
